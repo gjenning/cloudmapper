f515d6f8789f6930516d0f09d969da733be9cab9
Mike Jennings <gjennings@gmjdesigns.co>
2021-02-16 22:44:18 -0500

    fixed issue with get_us_east_1, changed it to mactch on us-gov-west-1 instead of returning us-gov-west-1

--- shared/common.py
+++ shared/common.py
@@ -326,13 +326,12 @@ def get_account_stats(account, all_resources=False):
 
 
 def get_us_east_1(account):
-    return "us-gov-west-1"
-    # for region_json in get_regions(account):
-    #     region = Region(account, region_json)
-    #     if region.name == "us-east-1":
-    #         return region
-    #
-    # raise Exception("us-east-1 not found")
+    for region_json in get_regions(account):
+        region = Region(account, region_json)
+        if region.name == "us-gov-west-1":
+            return region
+
+    raise Exception("us-gov-west-1 not found")
 
 
 def iso_date(d):
